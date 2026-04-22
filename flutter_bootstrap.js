a2f5759bad86f6d936319726be2e7c58c1f5a538
jhoveraraque <jhoveraraque@icloud.com>
2026-04-22 10:02:44 -0500

    Fix nombre

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3645545678" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4000046940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
